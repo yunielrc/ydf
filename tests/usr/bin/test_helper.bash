d7f43ee1b1707ef5d2b42594904086f8de1781cf
yunielrc <yunielrc87@gmail.com>
2023-10-07 22:11:10 -0400

    test(ydf): move ydf functional test suite to bin

--- tests/usr/bin/test_helper.bash
+++ tests/usr/bin/test_helper.bash
@@ -1,4 +1,2 @@
-# shellcheck source=../../test_helper_base.bash
-. "${SRC_DIR}/test/test_helper_base.bash"
-# shellcheck source=../../../usr/bin/ydf
-. "${SRC_DIR}/usr/bin/ydf"
+# shellcheck source=../../../test_helper_base.bash
+. "${TESTS_DIR}/test_helper_base.bash"
